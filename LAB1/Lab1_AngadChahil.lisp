7f4b2c03cf309423cfad8458a3a4dade5e3786e1
AngadSC <104170661+AngadSC@users.noreply.github.com>
2026-01-13 22:22:46 -0700

    finished function 1 just gotta test

--- LAB1/Lab1_AngadChahil.lisp
+++ LAB1/Lab1_AngadChahil.lisp
@@ -1,1 +1,16 @@
-; Question 1 
+#| Question 1
+The function returns the count of all the non-numeric atoms wihtin a list, including all the ones 
+in a nested list.
+|#
+(defun count0 (L)
+    (if ( null L)    
+    0
+    (if (atom( car L))
+        (if (numberp (car L))
+            (+0 (count0 (cdr L)))
+            ( +1 (count0 (cdr L))))
+        ( + (count0 (car L)) (count0 (cdr L))))))
+
+;test cases
+(count0 '(a 1 (b 2 )))
+(count0 '(a 1 3 4 (3 a j (b 2))))
