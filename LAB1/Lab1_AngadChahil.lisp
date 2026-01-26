9678545b17f51d48b1376e56dd98260db2803201
AngadSC <angads.chahil@gmail.com>
2026-01-25 17:27:39 -0700

    bug fixes

--- LAB1/Lab1_AngadChahil.lisp
+++ LAB1/Lab1_AngadChahil.lisp
@@ -264,7 +264,7 @@ SO find all pairs that are X,Y - > and return a list of the Ys.
 |#
 
 (defun reached (x L)
-    (remove-dup (reached-help (list x) L '())))
+    (remove-dups (reached-help (list x) L '())))
 
 ; helper functin that uses accumulator for the pages that have been visited
 (defun reached-help (to-visit L visited)
