87f0da2205ab8f95d10615651e18140d274146f2
AngadSC <104170661+AngadSC@users.noreply.github.com>
2026-02-18 22:27:07 -0700

    needs ttesting

--- LAB2/1757558.lisp
+++ LAB2/1757558.lisp
@@ -43,3 +43,199 @@
                     (fl-lookup-def fname arg (cdr p)))))))
 
 
+(defun fl-user-fun-p (sym arg p)            
+  (and (symbolp sym)                            
+       (fl-lookup-def sym arg p))) 
+
+;go through the pairs until we find a paiur that = the key, and recurse til we do 
+; so given a var we find the value it algins to 
+(defun fl-assoc (key alist)
+  (cond
+    ((null alist) nil)
+    ((eq (caar alist) key) (car alist))
+    (t (fl-assoc key (cdr alist)))))
+
+
+; this is for making suyre when we use a lambda function we dont use vars from outside and stay with the ones we defined 
+
+(defun fl-remove-keys (keys alist)
+  (cond
+    ((null alist) nil)
+    ((member (caar alist) keys :test #'eq)
+     (fl-remove-keys keys (cdr alist)))
+    (t (cons (car alist)
+             (fl-remove-keys keys (cdr alist))))))
+
+
+; subsitrutes in symbols 
+(defun fl-subst (expr env)                      
+  
+  (cond
+   
+    ((atom expr)                                ; if expression is an atom 
+     (cond
+       ((or (eq expr t) (null expr)) expr)         ; keeps the bools the csame 
+       ((numberp expr) expr)                        ; if its s asymbol we might need to replace 
+       ((symbolp expr)                             
+        (let ((pair (fl-assoc expr env)))          
+          (if pair (cdr pair) expr)))             ; reoklace with value, else keep symbol as is 
+       (t expr)))                                 
+
+    
+    ((and (consp expr) (eq (car expr) 'quote))     
+     expr)                                        
+
+   
+    ((fl-lambda-p expr)                           ; checks if exprssions is lambda 
+     (let* ((params (cadr expr))                  
+            (body (caddr expr))                   ; gets teh param kust and body 
+            (env2 (fl-remove-keys params env)))   
+       (list 'lambda params                        ; remakes thge lamda expresions 
+             (fl-subst body env2))))             
+
+    
+    (t (mapcar (lambda (x) (fl-subst x env)) expr)))) 
+
+
+;evalutes an argyuement expression 
+(defun fl-eval-args (a p)
+    (cond 
+    ((null a) nil)                  ; iuf we dont have aby retyurns empty lsit 
+        (t (cons (fl-interp (car a) p)      ; eautes the fitst arguement adn recursees 
+            (fl-eval-args (cdr a) p)))))
+
+
+; applying the primtives 
+
+(defun fl-apply-prim (op a p)
+ 
+  (cond
+  ; checks if its a qute 
+    ((eq op 'quote)
+    
+     (car a))
+;checks if its IF, evlautes tonly if we have condition and returns the branch 
+    ((eq op 'if)
+     (let ((c (fl-interp (first a) p)))
+       (if c
+           (fl-interp (second a) p)
+           (fl-interp (third a) p))))
+
+;checks for COND,  extracts both parts and calls the eval 
+    ((eq op 'cond)
+    (labels ((eval-clauses (cls)
+        (cond 
+            ((null cls) nil)
+            (t 
+            (let* ((cl  (car cls))
+            (test (car cl))
+            (expr (cadr cl)))
+        
+            (if (fl-interp test p)
+                (fl-interp expr p)
+                    (eval-clauses (cdr cls))))))))
+    
+    (eval-clauses a)))
+
+; evalutes for and 
+; checks for short circuit like if false then we switch to nul 
+    ((eq op 'and)
+    (labels ((go (xs)
+        (cond   
+            ((null xs) t)
+                (t 
+                    (let ((v (fl-interp (car xs) p)))
+                        (if v (go (cdr xs)) nil))))))
+    
+    (go args)))
+
+; for OR 
+; has recruse for short cituit too, 
+; if one part returns as true returns, else it trires the nextzxs 
+
+((eq op 'or)                                  
+     (labels ((go (xs)                              
+                (cond
+                  ((null xs) nil)                  
+                  (t
+                   (let ((v (fl-interp (car xs) prog))) 
+                     (if v v (go (cdr xs))))))))    
+       (go args)))    
+
+
+
+;lambda applicaiton 
+; applies the lamdba expression  LAM 
+; extracts param list from the body 
+; 
+;
+(defun fl-apply-lambda (lam arg-exprs p) 
+    (let* ((params (cadr lam))
+            (body  (caddr lam))
+            (evargs (fl-eval-args arg-exprs p))
+            (env (fl-pairlis params evargs))
+            (bodt2 (fl-subst body env))
+            (fl-interp body2 p))))))
+
+
+; main inteprerter 
+
+(defun fl-interp (E P)
+(cond
+; if its an atom itself 
+    ((atom E) E)
+    
+    (t
+    (let* ((f(car E))
+        (args (cdr E)))
+      ; if operato is primtive then we recongize it an apploy the primaitve operatio nit needs by helper funciton aboe   
+      (cond 
+        ((and (symbolp f) (fl-primop-p f))
+        (fl-apply-prim f args P)))
+
+     ; for a defined ufnciton , substitues the function into the body as needed 
+            ((and (symbolp f) (fl-user-fun-p f (length args) P)) 
+          (let* ((def (fl-lookup-def f (length args) P))      
+                 (params (cadr def))                           
+                 (body (cadddr def))                          
+                 (evargs (fl-eval-args args P))                
+                 (env (fl-pairlis params evargs))              
+                 (body2 (fl-subst body env)))                
+            (fl-interp body2 P)))    
+; direct application for lambda 
+             ((fl-lambda-p f)  
+             (fl-apply-lambda f args P))  
+             ((consp f)                                   
+          (let ((fv (fl-interp f P)))                
+            (cond
+              ((fl-lambda-p fv)                         
+               (fl-apply-lambda fv args P))           
+              ((symbolp fv)                            
+               (fl-interp (cons fv args) P))           
+              (t
+               E))))                                   
+
+       ; undefined operator 
+         (t E)))))          
+
+
+
+;;;; ----------------------------
+;;;; Example program from your notes (optional)
+;;;; ----------------------------
+
+(defparameter *user-defined-progs*                    ; Define a global variable holding sample user-defined FL functions.
+  '(
+    (double (x) = (+ x x))                           ; double(x) = x + x
+    (quadruple (x) = (double (double x)))            ; quadruple(x) = double(double(x))
+    (my-len (L) = (if (null L) 0 (+ 1 (my-len (cdr L))))) ; length(L) recursively
+    (my-member (x L) = (if (null L) nil (if (equal x (car L)) T (my-member x (cdr L))))) ; membership test
+    (my-append (L1 L2) = (if (null L1) L2 (cons (car L1) (my-append (cdr L1) L2)))) ; append
+    (my-reverse (L) = (if (null L) nil (my-append (my-reverse (cdr L)) (cons (car L) nil)))) ; reverse
+    (fib (n) = (if (< n 2) n (+ (fib (- n 1)) (fib (- n 2))))) ; fibonacci
+    (range (n) = (if (= n 0) nil (cons n (range (- n 1))))) ; range down to 1
+    (my-map (F L) = (if (null L) nil (cons (F (car L)) (my-map F (cdr L))))) ; map function over list
+    (my-filter (P L) = (if (null L) nil (if (P (car L)) (cons (car L) (my-filter P (cdr L))) (my-filter P (cdr L))))) ; filter
+    (my-reduce (F Acc L) = (if (null L) Acc (my-reduce F (F Acc (car L)) (cdr L)))))) ; reduce/fold
+
+    
