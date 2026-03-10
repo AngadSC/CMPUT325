7fb403ef5b48d57016308920f8afbdc7ce68b4bc
AngadSC <angads.chahil@gmail.com>
2026-03-10 16:23:17 -0600

    question 1 is done

--- Lab3/1757558.pl
+++ Lab3/1757558.pl
@@ -1,1 +1,16 @@
-# Question  1 , checking git 
+# Question  1 
+# SO S3 is the set difference betweeen S2 and S1. 
+# base case would be empty S1, maybe use a cut to not evalute S2 in taht case 
+# we can like recurse through S1 and just run it with member on the rest of S2
+# if true is member then we excldue, if false we append the head we took from S1 into S3 
+
+# base case 
+setDiff([],_,[]).
+
+setDiff([H|T],S2,S3) :- 
+    member(H,S2),
+    setDiff(T,S2,S3).
+
+setDiff([H|T], S2, [H|S3]) :-
+    \+ member(H,S2),
+    setDiff(T,S2,S3).
