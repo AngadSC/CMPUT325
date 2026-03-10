313f28f143c68e5f2467cba9f8807fe2d1077c3c
AngadSC <angads.chahil@gmail.com>
2026-03-10 17:21:11 -0600

    questions 2 need to test

--- Lab3/1757558.pl
+++ Lab3/1757558.pl
@@ -1,10 +1,10 @@
-# Question  1 
-# SO S3 is the set difference betweeen S2 and S1. 
-# base case would be empty S1, maybe use a cut to not evalute S2 in taht case 
-# we can like recurse through S1 and just run it with member on the rest of S2
-# if true is member then we excldue, if false we append the head we took from S1 into S3 
+% Question  1 
+% SO S3 is the set difference betweeen S2 and S1. 
+% base case would be empty S1, maybe use a cut to not evalute S2 in taht case 
+% we can like recurse through S1 and just run it with member on the rest of S2
+% if true is member then we excldue, if false we append the head we took from S1 into S3 
 
-# base case 
+% base case 
 setDiff([],_,[]).
 
 setDiff([H|T],S2,S3) :- 
@@ -14,3 +14,16 @@ setDiff([H|T],S2,S3) :-
 setDiff([H|T], S2, [H|S3]) :-
     \+ member(H,S2),
     setDiff(T,S2,S3).
+
+
+% Question 2, 
+
+% base case
+exchange ([],[]).
+
+% a list with only one elment itll be the basecase if list is odd
+
+exchange([A],[A]).
+
+exchange([A,B|T], [B,A|R]) :-
+    exchange(T,R).
